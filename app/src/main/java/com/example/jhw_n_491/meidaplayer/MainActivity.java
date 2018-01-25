baf834cd87964484046fd5a69399dd3831b84d73
JiHoon Yoo <jh.yu@em-tech.co.kr>
2018-01-25 19:10:22 +0900

    [Music Player] Add seek function
    1. Improved media player function
    2. Sync music and seekbar
    3. Fix service termination problem
    4. Fix service binding error problem
    
    Change-Id: If59e330340fc04534ff0becd50a6774244da96e0

--- app/src/main/java/com/example/jhw_n_491/meidaplayer/MainActivity.java
+++ app/src/main/java/com/example/jhw_n_491/meidaplayer/MainActivity.java
@@ -1,29 +1,65 @@
 package com.example.jhw_n_491.meidaplayer;
 
 import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // Compents
     ImageView cImage;
-    Button btn_play, btn_stop;
+    Button btn_play, btn_stop, btn_forward, btn_backward;
     SeekBar seekbar_playtime;
 
+    // Status Action
     final String PLAY_BUTTON = "FOREGROUND_PLAY";
     final String STOP_BUTTON = "FOREGROUND_STOP";
+    final String FORWARD_BUTTON = "FOREGROUND_FORWARD";
+    final String BACKWARD_BUTTON = "FOREGROUND_BACKWARD";
+    final String SEEKBAR_DOWN = "SEEKBAR_DOWN";
+    final String SEEKBAR_UP = "SEEKBAR_UP";
 
-    Intent play_intent, stop_intent;
-    PendingIntent play_pending, stop_pending;
+    // Service Object
+    Intent play_intent, stop_intent, forward_intent, backward_intent, sbup_intent, sbdown_intent;
+    PendingIntent play_pending, stop_pending, forward_pending, backward_pending, sbup_pending, sbdown_pending;
+
+    // messenger
+    private Messenger mServiceMessenger = null;
+    private boolean mIsBound;
+
+    // Seekbar Time
+    int seekbar_time = 0;
+    int sync_time = 0;
+    String seekbar_status;
+
+
+    // Thread & Bind flag
+    private boolean thread_flag = false;
+    private boolean isBound = false;
+
+    // forward, backward flag
+    public static boolean wardflag = false;
+    public static String wardString = "";
+    Thread sampleThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,26 +71,83 @@ public class MainActivity extends AppCompatActivity {
         GlideDrawableImageViewTarget gifImage = new GlideDrawableImageViewTarget(cImage);
         Glide.with(this).load(R.drawable.backgrounds).into(gifImage);
 
+        setStartService();
+        runThread();
     }
 
     void initUiComponents()
     {
+        // Components init
         BtnOnClickListener onClickListener = new BtnOnClickListener();
+        BtnTouchEvent onTuchListener = new BtnTouchEvent();
+        SeekBarClickListener onChangeListener = new SeekBarClickListener();
+
+        sampleThread = new wardThread();
+        sampleThread.start();
+
         cImage = (ImageView)findViewById(R.id.gif_image);
         btn_play = (Button) findViewById(R.id.btn_play);
         btn_play.setOnClickListener(onClickListener);
         btn_stop = (Button) findViewById(R.id.btn_stop);
         btn_stop.setOnClickListener(onClickListener);
-        seekbar_playtime = (SeekBar) findViewById(R.id.seekbar_paytime);
 
+        // Touch Listener
+        btn_forward = (Button) findViewById(R.id.btn_forward);
+        btn_forward.setOnTouchListener(onTuchListener);
+        btn_backward = (Button) findViewById(R.id.btn_backward);
+        btn_backward.setOnTouchListener(onTuchListener);
+
+        seekbar_playtime = (SeekBar) findViewById(R.id.seekbar_paytime);
+        seekbar_playtime.setMax(0);
+        seekbar_playtime.setOnTouchListener(onChangeListener);
+
+        // Button intent init
         play_intent = new Intent(PLAY_BUTTON);
         stop_intent = new Intent(STOP_BUTTON);
+        forward_intent = new Intent(FORWARD_BUTTON);
+        backward_intent = new Intent(BACKWARD_BUTTON);
+        sbup_intent = new Intent(SEEKBAR_UP);
+        sbdown_intent = new Intent(SEEKBAR_DOWN);
 
+        // Button Pending Intent init
         play_pending = PendingIntent.getService(getApplicationContext(), 0, play_intent, 0);
         stop_pending = PendingIntent.getService(getApplicationContext(), 0 , stop_intent, 0);
-
     }
 
+    class SeekBarClickListener implements SeekBar.OnTouchListener{
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (v.getId())
+            {
+                case R.id.seekbar_paytime:
+                    if(event.getAction() == MotionEvent.ACTION_DOWN)
+                    {
+                        try {
+                            sbdown_intent.putExtra("seekbar","SEEKBAR_DOWN");
+                            sbdown_pending = PendingIntent.getService(getApplicationContext(),0,sbdown_intent,0);
+                            sbdown_pending.send(getApplicationContext(),0,sbdown_intent);
+                        } catch (PendingIntent.CanceledException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    else if(event.getAction() == MotionEvent.ACTION_UP)
+                    {
+                        try {
+                            sync_time = seekbar_playtime.getProgress();
+                            sbup_intent.putExtra("seekbar","SEEKBAR_UP");
+                            sbup_intent.putExtra("sbposition",sync_time);
+                            sbup_pending = PendingIntent.getService(getApplicationContext(),0,sbup_intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                            sbup_pending.send(getApplicationContext(),0,sbup_intent);
+                        } catch (PendingIntent.CanceledException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+            }
+            return false;
+        }
+    }
 
     class BtnOnClickListener implements Button.OnClickListener{
 
@@ -76,7 +169,249 @@ public class MainActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                     break;
+                default:
+                    break;
             }
         }
+    }
+
+    class BtnTouchEvent implements Button.OnTouchListener{
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch(v.getId())
+            {
+                case R.id.btn_forward:
+                    if(event.getAction() == MotionEvent.ACTION_DOWN)
+                    {
+                        forward_intent.putExtra("forward","FORWARD_DOWN");
+                        forward_pending = PendingIntent.getService(getApplicationContext(),0,forward_intent,0);
+                        wardflag = true;
+                        wardString = "forward";
+                    }
+                    else if(event.getAction() == MotionEvent.ACTION_UP)
+                    {
+                        try {
+                            forward_intent.putExtra("forward","FORWARD_UP");
+                            forward_pending = PendingIntent.getService(getApplicationContext(),0,forward_intent,0);
+                            forward_pending.send(getApplicationContext(),0,forward_intent);
+                        } catch (PendingIntent.CanceledException e) {
+                            e.printStackTrace();
+                        }
+                        wardflag = false;
+                        wardString ="";
+                    }
+                    break;
+                case R.id.btn_backward:
+                    if(event.getAction() == MotionEvent.ACTION_DOWN)
+                    {
+                        backward_intent.putExtra("backward","BACKWARD_DOWN");
+                        backward_pending = PendingIntent.getService(getApplicationContext(), 0, backward_intent, 0);
+                        wardflag = true;
+                        wardString = "backward";
+                    }
+                    else if(event.getAction() == MotionEvent.ACTION_UP)
+                    {
+                        try {
+                            backward_intent.putExtra("backward","BACKWARD_UP");
+                            backward_pending = PendingIntent.getService(getApplicationContext(), 0, backward_intent, 0);
+                            backward_pending.send(getApplicationContext(),0,backward_intent);
+                        } catch (PendingIntent.CanceledException e) {
+                            e.printStackTrace();
+                        }
+                        wardflag = false;
+                        wardString = "";
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        }
+    }
+
+    // service start, stop, connection
+    private void setStartService()
+    {
+
+        startService(new Intent(MainActivity.this, MusicService.class));
+        isBound = bindService(new Intent(this, MusicService.class), mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private void setStopService()
+    {
+        if(mIsBound)
+        {
+            unbindService(mConnection);
+            mIsBound = false;
+        }
+        stopService(new Intent(MainActivity.this, MusicService.class));
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mServiceMessenger = new Messenger(service);
+
+            try {
+                Message msg = Message.obtain(null, MusicService.MSG_REGISTER_CLIENT);
+                msg.replyTo = mMessenger;
+                mServiceMessenger.send(msg);
+
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+
+    public class wardThread extends Thread
+    {
+        public void run()
+        {
+            while(true)
+            {
+                if(wardflag)
+                {
+                    if(wardString.contains("forward"))
+                    {
+                        try {
+                            forward_pending.send(getApplicationContext(),0,forward_intent);
+                            sync_time++;
+                            seekbar_playtime.setProgress(sync_time);
+                        } catch (PendingIntent.CanceledException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    else if(wardString.contains("backward")) {
+                        try {
+                            backward_pending.send(getApplicationContext(), 0, backward_intent);
+                            if(sync_time != 0)
+                            {
+                                sync_time--;
+                            }
+                            seekbar_playtime.setProgress(sync_time);
+                        } catch (PendingIntent.CanceledException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void runThread() {
+
+        new Thread() {
+            public void run() {
+                while (true) {
+                    try {
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                if(thread_flag && sync_time <= seekbar_playtime.getMax())
+                                {
+                                    sync_time++;
+                                    if(sync_time == seekbar_playtime.getMax())
+                                    {
+                                        thread_flag = false;
+                                        sync_time = 0;
+                                    }
+                                    seekbar_playtime.setProgress(sync_time);
+                                }
+                            }
+                        });
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+    }
+
+
+    // Service로 부터 message를 받음
+    private final Messenger mMessenger = new Messenger(new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what) {
+                case MusicService.MSG_SEND_TO_ACTIVITY:
+                    seekbar_time = msg.getData().getInt("fromService");
+                    seekbar_playtime.setMax(seekbar_time);
+                    break;
+                case MusicService.MSG_SEND_SEEKBAR:
+                    seekbar_status = msg.getData().getString("fromSeekbar");
+                    if(seekbar_status.contains("PLAY"))
+                    {
+                        if(thread_flag)
+                        {
+                            thread_flag = false;
+                        }
+                        else
+                        {
+                            thread_flag = true;
+                        }
+                    }
+                    else if(seekbar_status.contains("STOP"))
+                    {
+                        thread_flag = false;
+                        sync_time = 0;
+                        seekbar_playtime.setProgress(0);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        }
+    }));
+
+    // Service로 Message 전송
+    private void sendMessageToService(String str) {
+        if (mIsBound) {
+            if (mServiceMessenger != null) {
+                try {
+                    Message msg = Message.obtain(null, MusicService.MSG_SEND_TO_SERVICE, str);
+                    msg.replyTo = mMessenger;
+                    mServiceMessenger.send(msg);
+                } catch (RemoteException e) {
+                }
+            }
+        }
+    }
+
+    // bind 해제
+    public void onBackPressed()
+    {
+        if(isBound)
+        {
+            unbindService(mConnection);
+            isBound = false;
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    protected  void onDestroy()
+    {
+        if(isBound)
+        {
+            unbindService(mConnection);
+            isBound = false;
+        }
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/example/jhw_n_491/meidaplayer/MusicService.java
+++ app/src/main/java/com/example/jhw_n_491/meidaplayer/MusicService.java
@@ -1,47 +1,71 @@
 package com.example.jhw_n_491.meidaplayer;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
 import android.os.PowerManager;
+import android.os.RemoteException;
+import android.util.Log;
 
 import java.io.IOException;
 
 public class MusicService extends Service{
 
+    // mediaplayer, notification
     MediaPlayer mMediaPlayer = null;
     MusicNotification music_notification;
 
+    // music prepard check
     boolean isPrepared;
 
-    // Notification & Media init
+    // Service Message\
+    public static final int MSG_REGISTER_CLIENT = 10;
+    public static final int MSG_SEND_TO_SERVICE = 11;
+    public static final int MSG_SEND_TO_ACTIVITY = 12;
+    public static final int MSG_SEND_SEEKBAR = 13;
+
+    // Activity 에서 가져온 Messenger
+    private Messenger mClient = null;
+    private int sbposition = 0;
+
     @Override
     public void onCreate()
     {
         super.onCreate();
 
-        //mMediaPlayer = new MediaPlayer();
-        music_notification = new MusicNotification(getApplicationContext());
-        music_notification.ExpandedlayoutNotification();
-        startForeground(music_notification.NOTIFICATION_PLAYER_ID, music_notification.mBuilder.build());
-
         Preparse_music();
+
+        music_notification = new MusicNotification(getApplicationContext());
 
         mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
                 isPrepared = true;
-                mp.start();
             }
         });
 
+        // 음악이 끝나고 다시 시작하기 위해 재설정
         mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
-                isPrepared = false;;
+                try {
+                    mp.stop();
+                    mp.reset();
+                    mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mp.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+                    mp.setDataSource(getApplicationContext(), Uri.parse("android.resource://com.example.jhw_n_491.meidaplayer/"+R.raw.rain));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mp.prepareAsync();
             }
         });
 
@@ -63,7 +87,8 @@ public class MusicService extends Service{
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        //throw new UnsupportedOperationException("Not yet implemented");
+        return mMessenger.getBinder();
     }
 
     // Service Action
@@ -81,35 +106,65 @@ public class MusicService extends Service{
                 }else
                 {
                     Play_music();
-
                 }
             }
             else if("FOREGROUND_STOP".equals(action))
             {
                 Stop_music();
             }
+            else if("FOREGROUND_FORWARD".equals(action))
+            {
+                if(intent.getStringExtra("forward").contains("FORWARD_DOWN"))
+                {
+                    if(isPlaying())
+                    {
+                        Pause_music();
+                    }
+                    mMediaPlayer.seekTo(mMediaPlayer.getCurrentPosition() + 1000);
+                }
+                else if(intent.getStringExtra("forward").contains("FORWARD_UP"))
+                {
+                    Play_music();
+                }
+            }
+            else if("FOREGROUND_BACKWARD".equals(action))
+            {
+                if(intent.getStringExtra("backward").contains("BACKWARD_DOWN"))
+                {
+                    if(isPlaying())
+                    {
+                        Pause_music();
+                    }
+                    mMediaPlayer.seekTo(mMediaPlayer.getCurrentPosition() - 1000);
+                }
+                else if(intent.getStringExtra("backward").contains("BACKWARD_UP"))
+                {
+                    Play_music();
+                }
+            }
+            else if("SEEKBAR_DOWN".equals(action))
+            {
+                if(intent.getStringExtra("seekbar").contains("SEEKBAR_DOWN"))
+                {
+                    if(isPlaying())
+                    {
+                        Pause_music();
+                    }
+                }
+
+            }
+            else if("SEEKBAR_UP".equals(action))
+            {
+                if(intent.getStringExtra("seekbar").contains("SEEKBAR_UP"))
+                {
+                    sbposition = intent.getIntExtra("sbposition",0);
+                    mMediaPlayer.seekTo((sbposition*1000));
+                    Play_music();
+                }
+            }
         }
 
         return START_NOT_STICKY;
-    }
-
-    // Destory Service
-    @Override
-    public void onDestroy()
-    {
-        if(mMediaPlayer != null)
-        {
-            mMediaPlayer.stop();
-            mMediaPlayer.release();
-            mMediaPlayer = null;
-
-            if(music_notification.mNotificationManager != null)
-            {
-                music_notification.mNotificationManager.cancel(0x342);
-                music_notification.mNotificationManager = null;
-                stopForeground(true);
-            }
-        }
     }
 
     // Playing music?
@@ -124,15 +179,30 @@ public class MusicService extends Service{
         if(isPrepared)
         {
             mMediaPlayer.start();
+            sendMsgToSEEKBAR("PLAY");
         }
+
+        music_notification.ExpandedlayoutNotification();
+        startForeground(music_notification.NOTIFICATION_PLAYER_ID, music_notification.mBuilder.build());
     }
 
     // Stop Music
     public void Stop_music()
     {
-        mMediaPlayer.stop();
-        mMediaPlayer.reset();
-        Preparse_music();
+        if(mMediaPlayer != null)
+        {
+            mMediaPlayer.stop();
+            mMediaPlayer.reset();
+            Preparse_music();
+            sendMsgToSEEKBAR("STOP");
+
+            if(music_notification.mNotificationManager != null)
+            {
+                music_notification.mNotificationManager.cancel(0x342);
+                music_notification.mNotificationManager = null;
+                stopForeground(true);
+            }
+        }
     }
 
     // Pause Music
@@ -141,6 +211,7 @@ public class MusicService extends Service{
         if(isPrepared)
         {
             mMediaPlayer.pause();
+            sendMsgToSEEKBAR("PLAY");
         }
     }
 
@@ -156,5 +227,49 @@ public class MusicService extends Service{
             e.printStackTrace();
         }
         mMediaPlayer.prepareAsync();
+    }
+
+    // Binder Setting
+    private final Messenger mMessenger = new Messenger(new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            Log.w("test","ControlService - message what : "+msg.what +" , msg.obj "+ msg.obj);
+            switch (msg.what)
+            {
+                case MSG_REGISTER_CLIENT:
+                    // activity로부터 가져온
+                    mClient = msg.replyTo;
+                    sendMsgToActivity((mMediaPlayer.getDuration()/1000));
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        }
+    }));
+
+    // Activity로 메시지 보내기
+    private void sendMsgToActivity(int sendValue)
+    {
+        try{
+            Bundle bundle = new Bundle();
+            bundle.putInt("fromService", sendValue);
+            Message msg = Message.obtain(null, MSG_SEND_TO_ACTIVITY);
+            msg.setData(bundle);
+            mClient.send(msg);      // msg 보내기
+        } catch (RemoteException e) {
+        }
+    }
+
+    private void sendMsgToSEEKBAR(String seekbar_status)
+    {
+        try{
+            Bundle bundle = new Bundle();
+            bundle.putString("fromSeekbar", seekbar_status);
+            Message msg = Message.obtain(null, MSG_SEND_SEEKBAR);
+            msg.setData(bundle);
+            mClient.send(msg);      // msg 보내기
+        } catch (RemoteException e) {
+        }
     }
 }
